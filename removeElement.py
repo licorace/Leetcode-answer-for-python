35934fd93024d31afc9f01d80402c007be3d995d
Blacksheep1995 <42202102+Blacksheep1995@users.noreply.github.com>
2019-04-09 16:53:31 +0800

    Update removeElement.py

--- removeElement.py
+++ removeElement.py
@@ -4,3 +4,12 @@ class Solution:
             if i == val:
                 nums.remove(i)
         return len(nums)
+    
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        j=len(nums)
+        for i in range(j-1,-1,-1):
+            if nums[i]==val:
+                nums.pop(i)    
+        return len(nums)
+    
