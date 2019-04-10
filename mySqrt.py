972a4894e374e6d34dc8da3afff8db2176ea4d1f
Blacksheep1995 <42202102+Blacksheep1995@users.noreply.github.com>
2019-04-10 19:44:05 +0800

    Create mySqrt.py

---
+++ mySqrt.py
@@ -0,0 +1,16 @@
+#二分法
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 1 or x == 0:
+            return x
+        high = x
+        low = 0
+        while low < high:
+            mid = low + (high - low)//2 
+            if mid * mid > x:    #如果mid的平方在x的右边
+                high = mid       #将mid的值赋给高值
+            elif mid * mid < x:
+                low = mid + 1    #如果mid的平方在x的左边，将mid值加一赋给低值
+            else:
+                return mid       #如果mid的平方恰好等于x，那恭喜我们找到了
+        return low-1             #如果没有找到正好相等的值，只能把最逼近的低值输出，由于之前低值是mid加了一的，故这里减去1
