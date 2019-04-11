e27bf7ad25f8c9661847515d0d5bcdd55445e993
Blacksheep1995 <42202102+Blacksheep1995@users.noreply.github.com>
2019-04-11 18:22:12 +0800

    Update maxDepth.py

--- maxDepth.py
+++ maxDepth.py
@@ -35,3 +35,29 @@ class Solution:
                 stack.append((current_depth + 1, root.right))
         
         return depth
+    
+#二叉树的层序建立法
+class TreeNode:
+    def __init__(self,x):
+        self.val = x
+        self.left = None
+        self.right = None
+    def BuildTree(self,nodeList):
+        if nodeList[0] == None:
+            return None
+        head = TreeNode(nodeList[0])
+        Nodes = [head]
+        j = 1
+        for node in Nodes:
+            if node != None:
+                node.left = (TreeNode(nodeList[j]) if nodeList[j] != None else None)
+                Nodes.append(node.left)
+                j += 1
+                if j == len(nodeList):
+                    return head
+                node.right = (TreeNode(nodeList[j]) if nodeList[j] != None else None)
+                j += 1
+                Nodes.append(node.right)
+                if j == len(nodeList):
+                    return head
+   
