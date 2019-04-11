70648504a0be9801fa8fd069c1a83cf0625b5274
Blacksheep1995 <42202102+Blacksheep1995@users.noreply.github.com>
2019-04-11 14:15:25 +0800

    Update isSymmetric.py

--- isSymmetric.py
+++ isSymmetric.py
@@ -21,3 +21,29 @@ class Solution:
             return False
           
 #迭代法
+#了递归的方法外，我们也可以利用队列进行迭代。队列中每两个连续的结点应该是相等的，而且它们的子树互为镜像。
+#最初，队列中包含的是 root 以及 root。该算法的工作原理类似于 BFS，但存在一些关键差异。
+#每次提取两个结点并比较它们的值。然后，将两个结点的左右子结点按相反的顺序插入队列中。
+#当队列为空时，或者我们检测到树不对称（即从队列中取出两个不相等的连续结点）时，该算法结束。
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        q = []
+        q.append(root)
+        q.append(root)
+        while len(q) != 0:   #即当队列q为空时退出循环
+            t1 = q.pop()
+            t2 = q.pop()
+            if not t1 and not t2:
+                continue
+            elif t1 == None or t2 == None:
+                return False
+            else:
+                if t1.val != t2.val:
+                    return False
+                else:
+                    q.append(t1.left)
+                    q.append(t2.right)
+                    q.append(t1.right)
+                    q.append(t2.left)
+        return True
+            
